d8b82320d91a7f6cb1e7ff52b90346998c8f1ce8
dgross881 <dgross881@gmail.com>
2014-08-25 14:47:36 +0800

    Add Authentication system

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  def require_user 
+    redirect_to sign_in_path unless current_user 
+  end 
   
   def signed_in? 
     current_user 
   end 
+  helper_method :signed_in?
 
   def current_user
-    return unless session[:user_id]
-    @current_user ||= User.find(session[:user_id])
+    User.find(session[:user_id]) if session[:user_id]
   end 
+  helper_method :current_user 
 end
