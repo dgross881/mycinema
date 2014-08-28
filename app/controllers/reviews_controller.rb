aa297d6d7f3760e173534842a886f6fed805bd67
dgross881 <dgross881@gmail.com>
2014-08-28 15:16:12 +0800

    add reviews#create controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,3 +1,22 @@
 class ReviewsController < ApplicationController
- 
-end
+  
+  def new 
+    @reviews = @video.review.new 
+  end 
+
+  def create 
+    @user = User.find_by(params[:id])
+    @review = @user.review.new(review_params) 
+    if @review.save
+      redirect_to video_path, notice: "Your review has been posted" 
+    else 
+      flash[:error] = "Your post was not posted" 
+     end 
+    end       
+  end
+
+  private 
+   def review_params 
+    params.require(:review).permit(:content, :rating) 
+   end 
+
