9f08b33a85fb451ea987f4c818ad9cac2c11b813
dgross881 <dgross881@gmail.com>
2014-09-16 18:37:19 +0800

    change relationship

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,4 @@
-# encoding: UTF-8
+# ncoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -56,5 +56,4 @@ ActiveRecord::Schema.define(version: 20140902021003) do
     t.datetime "updated_at"
     t.integer  "category_id"
   end
-
 end
