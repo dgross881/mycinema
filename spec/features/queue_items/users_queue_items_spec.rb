e4b1267747fd6cee2de799bbb545ef718de17248
dgross881 <dgross881@gmail.com>
2014-09-16 22:32:12 +0800

    Clean up queue_items feature spec

--- spec/features/queue_items/users_queue_items_spec.rb
+++ spec/features/queue_items/users_queue_items_spec.rb
@@ -8,20 +8,39 @@ feature "Users queue items" do
     familyguy = Fabricate(:video, title: "family guy", category: comedy)
     user = Fabricate(:user)
     sign_in user  
-    find("a[href='/videos/#{southpark.id}']").click
-    expect(page).to have_content(southpark.title)
     
-    click_link "+ My Queues" 
+    add_video_to_queue(southpark)
     expect(page).to have_content("#{southpark.title} was successfully added to your queue")
     
     visit video_path(southpark)
     expect(page).to_not have_content "+ My Queues"
 
+    add_video_to_queue(futurama)
+    add_video_to_queue(familyguy)
+
+    set_video_position(familyguy, 2)
+    set_video_position(southpark, 3)
+    set_video_position(futurama, 1)
+    
+    click_button "Update Instant Queue"
+    
+    expect(user.queue_items.first.video_id).to eq(futurama.id) 
+    expect(user.queue_items.second.video_id).to eq(familyguy.id)
+    expect(user.queue_items.third.video_id).to eq(southpark.id)
+  end 
+end 
+  
+  def set_video_position(video, position)
+    within dom_id_for video do
+      fill_in "queue_items[][position]", with: position 
+    end 
+  end 
+  
+  def add_video_to_queue(video)
     visit home_path 
-    find("a[href='/videos/#{futurama.id}']").click
-    click_link "+ My Queues" 
-    visit home_path 
-    find("a[href='/videos/#{familyguy.id}']").click
+    find("a[href='/videos/#{video.id}']").click
     click_link "+ My Queues" 
   end 
-end  
+  
+  #expect(find(:xpath, "//tr[contains(.,'#{southpark.title}')]//input[@type= 'text']").value).to eq("3")
+ 
