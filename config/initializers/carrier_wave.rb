183eea3a051e815752063e16e14bdbf1beb8194d
dgross881 <dgross881@gmail.com>
2014-10-29 11:52:48 +0800

    update carrier

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -4,10 +4,10 @@ CarrierWave.configure do |config|
     config.storage = :fog 
     config.fog_credentials = {
       :provider               => 'AWS',                        # required
-      :aws_access_key_id      => ENV['aws_access_key_id'],
-      :aws_secret_access_key  => ENV['aws_secret_access_key'], 
+      :aws_access_key_id      => <%= ENV['aws_access_key_id'] %>,
+      :aws_secret_access_key  => <%= ENV['aws_secret_access_key'] %>, 
     }
-    config.fog_directory  = ENV['aws_directory']                          # required
+    config.fog_directory  = <%= ENV['aws_directory'] %>                          # required
   else 
     config.storage = :file 
     config.enable_processing = Rails.env.development?
